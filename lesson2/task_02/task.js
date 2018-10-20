8639df79054b5a1bbca6c6e95fdaff5692d8e855
maksymchak <maksym4ak@gmail.com>
2018-10-20 21:04:52 +0300

    lesson2 level_02 finish

---
+++ lesson2/task_02/task.js
@@ -0,0 +1,37 @@
+const ArticleComponent = React.createClass({
+    render() {
+    	const {
+    		title,
+    		author,
+    		text,
+    		likes,
+    		comments
+    	} = this.props;
+
+        return (
+            <div>
+                <h1>{title}</h1>
+                <span>by {author}</span>
+                <p>{text}</p>
+                <br/>
+                <div className="stats">
+                    <i className="stat-icon fa fa-heart" />
+                    {likes}
+                    <i className="stat-icon fa fa-comment" />
+                    {comments}
+                </div>
+            </div>            
+        );
+    }
+});
+
+ReactDOM.render(
+    <ArticleComponent
+		title="Article title"
+		author=" Maksymchak"
+    	text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ex ad sapiente. Aut reprehenderit odit nam, repellat aliquam ut est eligendi voluptate velit voluptatum, ullam aliquid a blanditiis officia eos."			
+    	likes={121}
+    	comments={34}
+    />,
+    document.getElementById('article')
+); 
