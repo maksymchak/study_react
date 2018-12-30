42bb4c5fb572996ef8a17247787a5eed1969c38c
maksymchak <maksym4ak@gmail.com>
2018-12-31 00:07:58 +0200

    lesson2 level_04_02 start

---
+++ lesson2/task_04_02/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import AppTitle from './AppTitle.js';
+import Contacts from './Contacts.js';
+
+const App = () => {
+  return (
+    <div>
+      <AppTitle />
+      <Contacts />
+    </div>
+  );
+};
+
+export default App;
