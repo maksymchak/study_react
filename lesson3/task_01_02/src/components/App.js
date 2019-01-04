8ea862a45185674a2815bef4d77680c8a1d18bf2
maksymchak <maksym4ak@gmail.com>
2019-01-05 00:03:36 +0200

    lesson3 level_01_02 add app style

--- lesson3/task_01_02/src/components/App.js
+++ lesson3/task_01_02/src/components/App.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 
+import './App.css';
+
 export default class App extends Component {
-  
+
+ 
+  render() {
+
+    return(
+      <div
+        className="circle"
+        style={{width: size, height: size}}
+        onClick={this.handleClick}>
+      </div>
+    );
+  };
 }
