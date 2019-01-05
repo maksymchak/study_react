779b6d51d28e0429935844133ecc47ee065e7fa3
maksymchak <maksym4ak@gmail.com>
2019-01-05 13:12:43 +0200

    lesson3 level_02_02 start

---
+++ lesson3/task_02_02/src/components/AppTitle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './AppTitle.css';
+
+const AppTitle = () => {
+  return <h1 className="headLink">Meet React Course Members</h1>
+};
+
+export default AppTitle;
---
+++ lesson3/task_02_02/src/components/ContactsItem.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import './ContactsItem.css';
+
+const ContactsItem = ( props ) => {
+  const {
+    avatar,
+    name,
+    location,
+    geoData,
+    facebook,
+    github,
+    mail
+  } = props;
+
+  return (
+    <div className="container">
+      <img
+        src={avatar}
+        className="avatar"
+        width="150"
+        height="150"
+        alt={name}
+        />
+      <div className="container__info">
+        <p className="container__info__name">{name}</p>
+        <a className="container__info__location" href={geoData}><i className="fa fa-map-marker" aria-hidden="true"></i> {location}</a>
+        <div>
+          <a className="container__info__link" href={facebook}>
+            <i className="fa fa-facebook-official" aria-hidden="true"></i>
+          </a>
+          <a className="container__info__link" href={github}>
+            <i className="fa fa-github" aria-hidden="true"></i>
+          </a>
+          <a className="container__info__link" href={mail}>
+            <i className="fa fa-envelope" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactsItem;
