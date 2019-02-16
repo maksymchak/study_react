6424d4963b91493c0ff1d6ab24d8715d1311cea6
maksymchak <maksym4ak@gmail.com>
2019-02-16 16:29:08 +0200

    lesson3 level_02_02 fix

--- lesson3/task_02_02/src/components/Contacts.js
+++ lesson3/task_02_02/src/components/Contacts.js
@@ -6,18 +6,14 @@ import ContactsItem from './ContactsItem.js';
 const Contacts = ({ users }) => {
 
   const elements = users.map((item) => {
+
+    const {id, ...itemProps} = item;
+
     return (
-      <ContactsItem 
-        key={item.id}
-        avatar={item.avatar}
-        name={item.name}
-        location={item.location}
-        geoData={item.geoData}
-        facebook={item.facebook}
-        github={item.github}
-        mail={item.mail}        
-      />
-    )
+      <div key={id}>
+        <ContactsItem {...itemProps} />
+      </div>
+    );
   });
 
   return (
